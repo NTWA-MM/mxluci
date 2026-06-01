e6007810677d61c0497e6bf99332898b2b2cf3b7
NTWA-MM <4yg99js2fc@privaterelay.appleid.com>
2026-06-01 06:47:33 +0630

    Add MainActivity.kt

---
+++ app/src/main/java/com/example/mxluci/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mxluci
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
